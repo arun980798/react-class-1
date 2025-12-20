c33513ed0a33a1385f580e897e6d7c68bbe6d8b4
arun980798 <arunrajput88176894@gmail.com>
2025-12-20 11:26:17 +0530

    task1 navbar done

---
+++ task1/src/component/landingpage/Landingpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Landingpage = () => {
+  return (
+    <div>
+ <Navbar/>
+      
+    </div>
+  )
+}
+
+export default Landingpage
---
+++ task1/src/component/landingpage/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+
+    <div className="navbar">
+    <div className="navledt">
+        <a href="">arunrajput88176894@gmail.com</a>
+        <button>copy</button>
+        <button>cv</button>
+      </div>
+      <div className="navright">
+        <a href="">linkdin/</a> 
+        <a href="">dribble/</a>
+        <a href="">instagram/</a>
+      </div>
+
+      
+    </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
