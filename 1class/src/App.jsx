7eab082bbe8868f1c85fcdc1e3dd1ae79ba32013
arun980798 <arunrajput88176894@gmail.com>
2025-12-20 10:25:23 +0530

    class 1 and task 1 file create

---
+++ 1class/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+<div className="h1">
+  <h1>hello</h1>
+</div>
+
+    </>
+  )
+}
+
+export default App
